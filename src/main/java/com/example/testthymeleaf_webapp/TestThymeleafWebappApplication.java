46cf926ef9bd081f1b5246b061e1c61ea280d65a
Евгений <eship89@gmail.com>
2022-11-25 18:29:44 +0500

    test

--- src/main/java/com/example/testthymeleaf_webapp/TestThymeleafWebappApplication.java
+++ src/main/java/com/example/testthymeleaf_webapp/TestThymeleafWebappApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestThymeleafWebappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestThymeleafWebappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TestThymeleafWebappApplication.class, args);
+    }
 
 }
