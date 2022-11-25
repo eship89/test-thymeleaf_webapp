d79bb3403e6a5f719969e8c45dd67e34df3213ef
Евгений <eship89@gmail.com>
2022-11-25 18:23:32 +0500

    Initial commit

---
+++ src/test/java/com/example/testthymeleaf_webapp/TestThymeleafWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testthymeleaf_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestThymeleafWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
